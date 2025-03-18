8e72d443b88e547cf23ddbaedbe5b40f2980acd4
Nobrega <nobregamf029@gmail.com>
2025-03-18 20:18:07 -0300

    Initial commit

---
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,14 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController extends AlunoService {
+
+    
+
+
+}
