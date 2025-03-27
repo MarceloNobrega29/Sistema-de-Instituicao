75e5dc122a532ed77d835d5f3fe2d153f6b2e516
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-03-26 21:48:55 -0300

    Adicionando as requicições GET, DELETE e PUT

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -1,11 +1,13 @@
 package br.com.alunoonline.api.controller;
 
 import br.com.alunoonline.api.model.Aluno;
-import br.com.alunoonline.api.repository.AlunoRepository;
 import br.com.alunoonline.api.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/alunos")
@@ -17,7 +19,29 @@ public class AlunoController {
         @PostMapping
         @ResponseStatus(HttpStatus.CREATED)
 
-        public void criarAluno (@RequestBody Aluno aluno){
+        public void criarAluno(@RequestBody Aluno aluno) {
                 alunoService.criarAluno(aluno);
         }
+
+        @GetMapping("/{id}")
+        @ResponseStatus(HttpStatus.OK)
+        public Optional<Aluno> obterPorId(@PathVariable Long id) {
+            return alunoService.buscarPorId(id);
+
+        }
+
+        @DeleteMapping(("/{id}"))
+        @ResponseStatus(HttpStatus.OK)
+        public void deletar(@PathVariable Long id) {
+            alunoService.deletarAluno(id);
+        }
+
+        @PutMapping("/{id}")
+        @ResponseStatus(HttpStatus.OK)
+        public void atualizarAluno(@PathVariable("id") Long id, @RequestBody Aluno aluno) {
+                aluno.setId(id);
+                alunoService.atualizarAluno(id, aluno);
+        }
+
+
 }
--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AlunoService {
 
@@ -12,6 +15,18 @@ public class AlunoService {
     AlunoRepository alunoRepository;
 
     public void criarAluno(Aluno aluno) {
+        alunoRepository.save(aluno);
+    }
+
+    public Optional<Aluno> buscarPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public void deletarAluno(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
+    public void atualizarAluno(Long id, Aluno aluno) {
         alunoRepository.save(aluno);
     }
 
