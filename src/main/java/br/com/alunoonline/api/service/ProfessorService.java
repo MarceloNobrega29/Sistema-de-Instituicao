d0a5cd69241b95d5b77f44c32d85d1cc227b0dd8
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-04-22 11:05:54 -0300

    feat: implementa listar todos professores via service

--- src/main/java/br/com/alunoonline/api/service/ProfessorService.java
+++ src/main/java/br/com/alunoonline/api/service/ProfessorService.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,5 +37,9 @@ public class ProfessorService {
 
         professorRepository.save(professorBancoDeDados);
     }
-    
+
+    public List<ProfessorModel> listarTodosProfessores() {
+        return professorRepository.findAll();
+    }
+
 }
