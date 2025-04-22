ca2479b2109ecd0b8c1874b494da0c20aa5a8842
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-04-22 11:43:27 -0300

    feat: adiciona endpoint para buscar professor por id

--- src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
+++ src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/professores")
 public class ProfessorController {
@@ -19,4 +21,10 @@ public class ProfessorController {
         professorService.criarProfessor(professorModel);
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<ProfessorModel> buscarProfessorPorId(@PathVariable Long idProfessor) {
+        return professorService.buscarProfessorPorId(idProfessor);
+    }
+    
 }
