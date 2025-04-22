8ce51243eb681edad650034b3e487fa45b7a2fa4
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-04-22 10:46:34 -0300

    feat: implementa buscar professor por id via service

--- src/main/java/br/com/alunoonline/api/service/ProfessorService.java
+++ src/main/java/br/com/alunoonline/api/service/ProfessorService.java
@@ -5,6 +5,8 @@ import br.com.alunoonline.api.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProfessorService {
 
@@ -14,4 +16,9 @@ public class ProfessorService {
     public void criarProfessor(ProfessorModel professorModel) {
         professorRepository.save(professorModel);
     }
+
+    public Optional<ProfessorModel> buscarProfessorPorId(Long idProfessor) {
+        return professorRepository.findById(idProfessor);
+    }
+    
 }
