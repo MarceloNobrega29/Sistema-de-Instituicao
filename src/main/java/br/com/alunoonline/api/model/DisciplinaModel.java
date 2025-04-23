2acfe5de8631b9b08ffc2eff4f8edfd20844f6ea
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-04-23 11:26:32 -0300

    feat: Criação do model de disciplina

---
+++ src/main/java/br/com/alunoonline/api/model/DisciplinaModel.java
@@ -0,0 +1,29 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "disciplina")
+@Entity
+public class DisciplinaModel {
+
+    @Id
+    @GeneratedValue(strategy = @GenerationType.IDENTITY)
+
+    private Long idDisciplina;
+
+    private String nomeDisciplina;
+
+    private Integer cargaHoraria;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private ProfessorModel professorModel;
+    
+}
