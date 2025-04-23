fc892794afeb33f5885b7f043acc49a184fbfa22
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-04-23 11:51:17 -0300

    feat: adiciona o método lsitarTodasDisciplinas no service

--- src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,5 +37,8 @@ public class DisciplinaService {
         disciplinaRepository.save(disciplinaBancoDeDados);
     }
 
+    public List<DisciplinaModel> listarTodasDisciplinas() {
+        return disciplinaRepository.findAll();
+    }
     
 }
