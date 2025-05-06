0c6f0900caac730b6729f7ed0a051f1e0344a0c7
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-05-06 05:06:44 -0300

    feat: criação da matricula model

---
+++ src/main/java/br/com/alunoonline/api/model/MatriculaModel.java
@@ -0,0 +1,32 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "matricula_aluno")
+@Entity
+public class MatriculaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Double nota1;
+
+    private Double nota2;
+
+    @Enumerated(EnumType.STRING)
+    private MatriculaAlunoStatusEnum status;
+
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id")
+    private DisciplinaModel disciplinaModel;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private AlunoModel alunoModel;
+}
