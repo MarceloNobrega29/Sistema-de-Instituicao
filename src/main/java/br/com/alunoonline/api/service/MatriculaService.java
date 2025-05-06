1e63f76ad1362605b0389c4c253e824b985f483f
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-05-06 05:26:58 -0300

    feat: adiciona o método criarMatricula no service

--- src/main/java/br/com/alunoonline/api/service/MatriculaService.java
+++ src/main/java/br/com/alunoonline/api/service/MatriculaService.java
@@ -1,5 +1,7 @@
 package br.com.alunoonline.api.service;
 
+import br.com.alunoonline.api.enuns.MatricukaAlunoStatusEnum;
+import br.com.alunoonline.api.model.MatriculaModel;
 import br.com.alunoonline.api.repository.MatriculaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,10 @@ public class MatriculaService {
 
     @Autowired
     MatriculaRepository matriculaRepository;
+
+    public void criarMatricula(MatriculaModel matriculaModel) {
+        matriculaModel.setStatus(MatricukaAlunoStatusEnum.MATRICULADO);
+        matriculaRepository.save(matriculaModel);
+    }
     
 }
