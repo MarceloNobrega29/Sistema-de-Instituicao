fc457ad378f61f365fc2494dfe73454aad69c90f
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-05-06 05:19:56 -0300

    feat: criação do enum de status da matricula do aluno

---
+++ src/main/java/br/com/alunoonline/api/enuns/MatricukaAlunoStatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.alunoonline.api.enuns;
+
+public enum MatricukaAlunoStatusEnum {
+    MATRICULADO,
+    APROVADO,
+    REPROVADO,
+    TRANCADO;
+}
--- src/main/java/br/com/alunoonline/api/model/MatriculaModel.java
+++ src/main/java/br/com/alunoonline/api/model/MatriculaModel.java
@@ -1,5 +1,6 @@
 package br.com.alunoonline.api.model;
 
+import br.com.alunoonline.api.enuns.MatricukaAlunoStatusEnum;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,7 +21,7 @@ public class MatriculaModel {
     private Double nota2;
 
     @Enumerated(EnumType.STRING)
-    private MatriculaAlunoStatusEnum status;
+    private MatricukaAlunoStatusEnum status;
 
     @ManyToOne
     @JoinColumn(name = "disciplina_id")
