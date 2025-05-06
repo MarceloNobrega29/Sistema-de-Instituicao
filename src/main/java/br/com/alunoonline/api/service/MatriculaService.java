20490b76ca2494f8e4dab58b323819f8586231bc
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-05-06 05:22:37 -0300

    feat: criação da matricula service

---
+++ src/main/java/br/com/alunoonline/api/service/MatriculaService.java
@@ -0,0 +1,13 @@
+package br.com.alunoonline.api.service;
+
+import br.com.alunoonline.api.repository.MatriculaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatriculaService {
+
+    @Autowired
+    MatriculaRepository matriculaRepository;
+    
+}
